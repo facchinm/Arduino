0b67044870bfc58531dec496edf1772a2dccf0ac
Martino Facchin <m.facchin@arduino.cc>
2017-09-06 12:29:28 +0200

    Convert C++ version of attachInterrupt into templates

--- hardware/arduino/avr/cores/arduino/Arduino.h
+++ hardware/arduino/avr/cores/arduino/Arduino.h
@@ -243,7 +243,8 @@ uint16_t makeWord(byte h, byte l);
 
 unsigned long pulseIn(uint8_t pin, uint8_t state, unsigned long timeout = 1000000L);
 unsigned long pulseInLong(uint8_t pin, uint8_t state, unsigned long timeout = 1000000L);
-void attachInterrupt(uint8_t interruptNum, void (*)(void*), int mode, void* params);
+
+#include "WInterrupts.h"
 
 void tone(uint8_t _pin, unsigned int frequency, unsigned long duration = 0);
 void noTone(uint8_t _pin);
